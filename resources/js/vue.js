fbfaada089e689e43e9a71d58b7752815b01643e
Neeiser <stortini.study@gmail.com>
2022-05-30 18:52:26 +0200

    example fix

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -4,6 +4,11 @@ window.Vue = require('vue');
 window.Axios = require('axios');
 
 import App from './views/App.vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+/* const router = new VueRouter() */
 
 const app = new Vue({
     el: '#app',
